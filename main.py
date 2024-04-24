b1ab7305e31c98cff8f5de7af046a9f071362169
luchkin-a-g <luchkin_a.g@mail.ru>
2024-04-24 15:05:51 +0300

    Update main.py
    Change Client class to Bot class

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import discord
+from discord.ext import commands
 
 from settings import settings
-
 from bot_logic import *
 
 # Переменная intents - хранит привилегии бота
@@ -9,24 +9,24 @@ intents = discord.Intents.default()
 # Включаем привелегию на чтение сообщений
 intents.message_content = True
 # Создаем бота в переменной client и передаем все привелегии
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='$', intents=intents)
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f'We have logged in as {bot.user}')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content.startswith('$hello'):
-        await message.channel.send("Hi!")
-    elif message.content.startswith('$bye'):
-        await message.channel.send("\\U0001f642")
-    elif message.content.startswith('$pass'):
-        new_pass = gen_pass(10)
-        await message.channel.send(new_pass)
-    else:
-        await message.channel.send(message.content)
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Привет! Я бот {bot.user}!')
 
-client.run(settings["TOKEN"])
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def gen_pass(ctx, count_chars = 10):
+    new_pass = gen_pass(count_chars)
+    await ctx.send(new_pass)
+
+
+bot.run(settings["TOKEN"])
