4b699493173c88238e6f2f6057ffd4653c6b4abc
luchkin-a-g <luchkin_a.g@mail.ru>
2024-04-24 14:15:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
-print('hello world!!')
+import discord
+
+from settings import settings
+
+from bot_logic import *
+
+# Переменная intents - хранит привилегии бота
+intents = discord.Intents.default()
+# Включаем привелегию на чтение сообщений
+intents.message_content = True
+# Создаем бота в переменной client и передаем все привелегии
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$hello'):
+        await message.channel.send("Hi!")
+    elif message.content.startswith('$bye'):
+        await message.channel.send("\\U0001f642")
+    elif message.content.startswith('$pass'):
+        new_pass = gen_pass(10)
+        await message.channel.send(new_pass)
+    else:
+        await message.channel.send(message.content)
+
+client.run(settings["TOKEN"])
